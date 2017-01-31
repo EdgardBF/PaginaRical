84c7e39ac18c70f321a60c211232b64b1557cbbc
Miguel009 <miguelfr0305@hotmail.com>
2017-01-30 16:17:44 -0800

    Se agregaron las tablas para colocar la informacion

--- js/main.js
+++ js/main.js
@@ -2,4 +2,9 @@ $(document).ready(function(){
     $('.slider').slider();
     $('.carousel.carousel-slider').carousel({fullWidth: true})
     $(".button-collapse").sideNav();
+    $('.materialboxed').materialbox();
+    $('.modal').modal();
+    $('ul.tabs').tabs();
+
+
 });
