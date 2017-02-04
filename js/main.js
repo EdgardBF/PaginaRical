5c5bf4f5e58af808032b3b945db0caf845337b8f
Miguel009 <miguelfr0305@hotmail.com>
2017-02-04 00:09:33 -0600

    Se agregaron 2 apis

--- js/main.js
+++ js/main.js
@@ -6,6 +6,35 @@ $(document).ready(function(){
     $('.modal').modal();
     $('ul.tabs').tabs();
     $('.carousel.carousel-slider').carousel({fullWidth: true});
-    
-
 });
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {zoom: 16});
+  var geocoder = new google.maps.Geocoder;
+  geocoder.geocode({'address': 'Ricaldone'}, function(results, status) {
+    if (status === google.maps.GeocoderStatus.OK) {
+      map.setCenter(results[0].geometry.location);
+      new google.maps.Marker({
+        map: map,
+        position: results[0].geometry.location
+      });
+    } else {
+      window.alert('Geocode was not successful for the following reason: ' +
+          status);
+    }
+  });
+}
+window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '113362692514424',
+      xfbml      : true,
+      version    : 'v2.8'
+    });
+    FB.AppEvents.logPageView();
+  };
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "js/sdk.js";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
